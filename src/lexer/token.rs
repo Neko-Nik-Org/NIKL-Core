ed887ce3ba667d26cc9f9840592fddb928084ea4
Neko Nik <nikhil@nekonik.com>
2025-05-17 13:59:44 +0530

    Enhance lexer: add support for new keywords (import, pub, as) and improve token handling for symbols and literals

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -4,6 +4,9 @@ pub enum TokenKind {
     Let,
     Const,
     Function,
+    Import,
+    Pub,
+    As,
     Spawn,
     Wait,
     Assign,
@@ -55,6 +58,7 @@ pub enum TokenKind {
     Comma,
     Colon,
     Arrow,
+    Dot,
 
     // Keywords
     Eof,
@@ -106,269 +110,170 @@ impl<'a> Lexer<'a> {
         next
     }
 
+    fn add_token(&mut self, tokens: &mut Vec<Token>, kind: TokenKind, col: usize) {
+        tokens.push(Token {
+            kind,
+            line: self.line,
+            column: col,
+        });
+    }
+
     pub fn tokenize(mut self) -> Result<Vec<Token>, LexError> {
         let mut tokens = Vec::new();
 
-        while let Some((idx, ch)) = self.chars.peek().copied() {
+        while let Some(&(idx, ch)) = self.chars.peek() {
             match ch {
                 // Skip whitespace
-                ' ' | '\t' | '\r' => {
-                    self.advance();
-                }
-                '\n' => {
+                ' ' | '\t' | '\r' | '\n' => {
                     self.advance();
                 }
 
-                // Comments
+                // Comments: //
                 '/' => {
                     self.advance();
-                    if let Some((_, '/')) = self.chars.peek().copied() {
-                        self.advance();
-                        while let Some((_, ch)) = self.chars.peek().copied() {
-                            if ch == '\n' {
+                    if let Some(&(_, '/')) = self.chars.peek() {
+                        // Consume till newline
+                        while let Some(&(_, c)) = self.chars.peek() {
+                            if c == '\n' {
                                 break;
                             }
                             self.advance();
                         }
                     } else {
-                        tokens.push(Token {
-                            kind: TokenKind::Divide,
-                            line: self.line,
-                            column: self.column,
-                        });
+                        self.add_token(&mut tokens, TokenKind::Divide, self.column);
                     }
                 }
 
-                // Symbols
+                // Multi-char operators and single-char symbols
                 '=' => {
+                    let col = self.column;
                     self.advance();
-                    if let Some((_, '=')) = self.chars.peek().copied() {
+                    if let Some(&(_, '=')) = self.chars.peek() {
                         self.advance();
-                        tokens.push(Token {
-                            kind: TokenKind::Equals,
-                            line: self.line,
-                            column: self.column,
-                        });
+                        self.add_token(&mut tokens, TokenKind::Equals, col);
                     } else {
-                        tokens.push(Token {
-                            kind: TokenKind::Assign,
-                            line: self.line,
-                            column: self.column,
-                        });
+                        self.add_token(&mut tokens, TokenKind::Assign, col);
                     }
                 }
-                '(' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::LeftParen,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                ')' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::RightParen,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                '{' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::LeftBrace,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                '}' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::RightBrace,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                '[' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::LeftBracket,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                ']' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::RightBracket,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                ',' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::Comma,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                '+' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::Add,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                '-' => {
-                    self.advance();
-                    if let Some((_, '>')) = self.chars.peek().copied() {
-                        self.advance();
-                        tokens.push(Token {
-                            kind: TokenKind::Arrow,
-                            line: self.line,
-                            column: self.column,
-                        });
-                    } else {
-                        tokens.push(Token {
-                            kind: TokenKind::Subtract,
-                            line: self.line,
-                            column: self.column,
-                        });
-                    }
-                }
-                '*' => {
-                    self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::Multiply,
-                        line: self.line,
-                        column: self.column,
-                    });
-                }
-                '<' => {
-                    self.advance();
-                    if let Some((_, '=')) = self.chars.peek().copied() {
-                        self.advance();
-                        tokens.push(Token {
-                            kind: TokenKind::LessThanOrEqual,
-                            line: self.line,
-                            column: self.column,
-                        });
-                    } else {
-                        tokens.push(Token {
-                            kind: TokenKind::LessThan,
-                            line: self.line,
-                            column: self.column,
-                        });
-                    }
-                }
-                '>' => {
-                    self.advance();
-                    if let Some((_, '=')) = self.chars.peek().copied() {
-                        self.advance();
-                        tokens.push(Token {
-                            kind: TokenKind::GreaterThanOrEqual,
-                            line: self.line,
-                            column: self.column,
-                        });
-                    } else {
-                        tokens.push(Token {
-                            kind: TokenKind::GreaterThan,
-                            line: self.line,
-                            column: self.column,
-                        });
-                    }
-                }
+
                 '!' => {
+                    let col = self.column;
                     self.advance();
-                    if let Some((_, '=')) = self.chars.peek().copied() {
+                    if let Some(&(_, '=')) = self.chars.peek() {
                         self.advance();
-                        tokens.push(Token {
-                            kind: TokenKind::NotEqual,
-                            line: self.line,
-                            column: self.column,
-                        });
+                        self.add_token(&mut tokens, TokenKind::NotEqual, col);
                     } else {
-                        return Err(LexError::UnexpectedChar('!', self.line, self.column));
+                        return Err(LexError::UnexpectedChar('!', self.line, col));
                     }
                 }
-                ':' => {
+
+                '<' => {
+                    let col = self.column;
                     self.advance();
-                    tokens.push(Token {
-                        kind: TokenKind::Colon,
-                        line: self.line,
-                        column: self.column,
-                    });
+                    if let Some(&(_, '=')) = self.chars.peek() {
+                        self.advance();
+                        self.add_token(&mut tokens, TokenKind::LessThanOrEqual, col);
+                    } else {
+                        self.add_token(&mut tokens, TokenKind::LessThan, col);
+                    }
                 }
+
+                '>' => {
+                    let col = self.column;
+                    self.advance();
+                    if let Some(&(_, '=')) = self.chars.peek() {
+                        self.advance();
+                        self.add_token(&mut tokens, TokenKind::GreaterThanOrEqual, col);
+                    } else {
+                        self.add_token(&mut tokens, TokenKind::GreaterThan, col);
+                    }
+                }
+
+                '-' => {
+                    let col = self.column;
+                    self.advance();
+                    if let Some(&(_, '>')) = self.chars.peek() {
+                        self.advance();
+                        self.add_token(&mut tokens, TokenKind::Arrow, col);
+                    } else {
+                        self.add_token(&mut tokens, TokenKind::Subtract, col);
+                    }
+                }
+
+                // Single char tokens
+                '(' => { self.advance(); self.add_token(&mut tokens, TokenKind::LeftParen, self.column -1); }
+                ')' => { self.advance(); self.add_token(&mut tokens, TokenKind::RightParen, self.column -1); }
+                '{' => { self.advance(); self.add_token(&mut tokens, TokenKind::LeftBrace, self.column -1); }
+                '}' => { self.advance(); self.add_token(&mut tokens, TokenKind::RightBrace, self.column -1); }
+                '[' => { self.advance(); self.add_token(&mut tokens, TokenKind::LeftBracket, self.column -1); }
+                ']' => { self.advance(); self.add_token(&mut tokens, TokenKind::RightBracket, self.column -1); }
+                ',' => { self.advance(); self.add_token(&mut tokens, TokenKind::Comma, self.column -1); }
+                '+' => { self.advance(); self.add_token(&mut tokens, TokenKind::Add, self.column -1); }
+                '*' => { self.advance(); self.add_token(&mut tokens, TokenKind::Multiply, self.column -1); }
+                ':' => { self.advance(); self.add_token(&mut tokens, TokenKind::Colon, self.column -1); }
+                '.' => { self.advance(); self.add_token(&mut tokens, TokenKind::Dot, self.column -1); }
 
                 // String literals
                 '"' => {
                     let start_col = self.column;
-                    self.advance(); // consume quote
+                    self.advance(); // consume opening quote
                     let mut value = String::new();
 
-                    while let Some((_, ch)) = self.chars.peek().copied() {
+                    while let Some(&(_, ch)) = self.chars.peek() {
                         if ch == '"' {
-                            self.advance();
+                            self.advance(); // consume closing quote
                             break;
                         }
                         value.push(ch);
                         self.advance();
                     }
 
-                    if !value.ends_with('"') && !self.input[idx..].contains('"') {
+                    // Check if closed properly
+                    if !self.input[idx..].contains('"') && !self.input[idx..].ends_with('"') {
                         return Err(LexError::UnterminatedString(self.line, start_col));
                     }
 
-                    tokens.push(Token {
-                        kind: TokenKind::StringLiteral(value),
-                        line: self.line,
-                        column: start_col,
-                    });
+                    self.add_token(&mut tokens, TokenKind::StringLiteral(value), start_col);
                 }
 
-                // Numbers
+                // Numbers (int or float)
                 '0'..='9' => {
                     let start_col = self.column;
-                    let mut value = String::new();
+                    let mut num_str = String::new();
                     let mut dot_count = 0;
 
-                    while let Some((_, ch)) = self.chars.peek().copied() {
+                    while let Some(&(_, ch)) = self.chars.peek() {
                         if ch == '.' {
                             dot_count += 1;
-                        }
-                        if !ch.is_digit(10) && ch != '.' {
+                            if dot_count > 1 {
+                                break;
+                            }
+                        } else if !ch.is_ascii_digit() {
                             break;
                         }
-                        value.push(ch);
+                        num_str.push(ch);
                         self.advance();
                     }
 
-                    if dot_count > 1 {
-                        return Err(LexError::InvalidNumber(value, self.line, start_col));
-                    }
-
                     if dot_count == 1 {
-                        tokens.push(Token {
-                            kind: TokenKind::FloatLiteral(value.parse().unwrap()),
-                            line: self.line,
-                            column: start_col,
-                        });
+                        match num_str.parse::<f64>() {
+                            Ok(f) => self.add_token(&mut tokens, TokenKind::FloatLiteral(f), start_col),
+                            Err(_) => return Err(LexError::InvalidNumber(num_str, self.line, start_col)),
+                        }
                     } else {
-                        tokens.push(Token {
-                            kind: TokenKind::IntegerLiteral(value.parse().unwrap()),
-                            line: self.line,
-                            column: start_col,
-                        });
+                        match num_str.parse::<i64>() {
+                            Ok(i) => self.add_token(&mut tokens, TokenKind::IntegerLiteral(i), start_col),
+                            Err(_) => return Err(LexError::InvalidNumber(num_str, self.line, start_col)),
+                        }
                     }
                 }
 
-                // Identifiers / keywords / booleans
+                // Identifiers, keywords, booleans
                 ch if ch.is_alphabetic() || ch == '_' => {
                     let start_col = self.column;
                     let mut ident = String::new();
-                    while let Some((_, ch)) = self.chars.peek().copied() {
+
+                    while let Some(&(_, ch)) = self.chars.peek() {
                         if ch.is_alphanumeric() || ch == '_' {
                             ident.push(ch);
                             self.advance();
@@ -378,20 +283,28 @@ impl<'a> Lexer<'a> {
                     }
 
                     let kind = match ident.as_str() {
+                        "import" => TokenKind::Import,
+                        "pub" => TokenKind::Pub,
+                        "as" => TokenKind::As,
+
                         "let" => TokenKind::Let,
                         "const" => TokenKind::Const,
-                        "if" => TokenKind::If,
-                        "elif" => TokenKind::ElseIf,
-                        "else" => TokenKind::Else,
-                        "and" => TokenKind::And,
-                        "or" => TokenKind::Or,
-                        "not" => TokenKind::Not,
-                        "return" => TokenKind::Return,
                         "fn" => TokenKind::Function,
                         "spawn" => TokenKind::Spawn,
                         "wait" => TokenKind::Wait,
+                        "return" => TokenKind::Return,
+
+                        "if" => TokenKind::If,
+                        "elif" => TokenKind::ElseIf,
+                        "else" => TokenKind::Else,
+
+                        "and" => TokenKind::And,
+                        "or" => TokenKind::Or,
+                        "not" => TokenKind::Not,
+
                         "True" => TokenKind::BooleanLiteral(true),
                         "False" => TokenKind::BooleanLiteral(false),
+
                         "Int" => TokenKind::Integer,
                         "Float" => TokenKind::Float,
                         "String" => TokenKind::String,
@@ -399,29 +312,20 @@ impl<'a> Lexer<'a> {
                         "Array" => TokenKind::Array,
                         "Tuple" => TokenKind::Tuple,
                         "HashMap" => TokenKind::HashMap,
+
                         _ => TokenKind::Identifier(ident),
                     };
 
-                    tokens.push(Token {
-                        kind,
-                        line: self.line,
-                        column: start_col,
-                    });
+                    self.add_token(&mut tokens, kind, start_col);
                 }
 
-                // Error
-                ch => {
+                _ => {
                     return Err(LexError::UnexpectedChar(ch, self.line, self.column));
                 }
             }
         }
 
-        tokens.push(Token {
-            kind: TokenKind::Eof,
-            line: self.line,
-            column: self.column,
-        });
-
+        self.add_token(&mut tokens, TokenKind::Eof, self.column);
         Ok(tokens)
     }
 }
